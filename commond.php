2377b8164fb30a62c4031a0ed79a275001afe947
Hoàng Thỏa <hoangthoa90hn@gmail.com>
2014-08-21 16:44:50 +0700

    Update commond.php

--- commond.php
+++ commond.php
@@ -13,3 +13,23 @@ function rmdir_recursive($dir) {
         }
         rmdir($dir);
     }
+    
+//replace file_get_contents
+
+$file = "http://www.example.com/my_page.php";
+if (function_exists('curl_version'))
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $file);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $content = curl_exec($curl);
+    curl_close($curl);
+}
+else if (file_get_contents(__FILE__) && ini_get('allow_url_fopen'))
+{
+    $content = file_get_contents($file);
+}
+else
+{
+    echo 'You have neither cUrl installed nor allow_url_fopen activated. Please setup one of those!';
+}
